107cbf7a29bdd4f6fd2ff9a488d10c4b39e85d31
s863608933 <863608933@qq.com>
2020-04-09 10:07:47 +0800

    孙宇测试

---
+++ src/sunyu.java
@@ -0,0 +1,21 @@
+package com.neuedu.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.neuedu.pojo.MenuData;
+import com.neuedu.pojo.SysMenu;
+
+public interface IMenuDao {
+
+	int count()throws ClassNotFoundException, SQLException;
+	
+	List<SysMenu> findAll(int pageSize, int currentPage) throws ClassNotFoundException, SQLException;
+
+	int insert(SysMenu menu)throws ClassNotFoundException, SQLException;
+
+	List<SysMenu> getMenuList(String parentId)throws ClassNotFoundException, SQLException;
+
+	List<MenuData> getMenuDataList(String id)throws ClassNotFoundException, SQLException;
+
+}
